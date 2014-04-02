c57c0f3122cb9e5db63205213b03ae1a41b3d9fb
Tangent 128 <Tangent128@gmail.com>
2014-04-02 01:17:55 -0400

    split js file

---
+++ flytable.js
@@ -0,0 +1,157 @@
+function setupFlyTable(element) {
+	
+	var table = {};
+	table.container = element;
+	table.recycle = [];
+	table.inUse = [];
+	table.sliceStart = 0;
+	table.sliceEnd = 0;
+
+	// extra space on either side to render
+	var SCROLL_BUFFER = 10;
+	
+	/* Public Overrides */
+	
+	table.recalculate = function() {}
+	
+	table.getComponent = function(index, recycle) {
+		if(!recycle) {
+			recycle = $("<s>no renderer</s>");
+		}
+		
+		return recycle;
+	}
+	
+	table.getItemTop = function(index) {
+		return 16*index; // arbitrary value
+	}
+	
+	table.getItemHeight = function(index) {
+		return 16; // arbitrary value
+	}
+	
+	table.getTotalHeight = function() {
+		return 160; // arbitrary value
+	}
+
+	table.pixelToIndex = function(y) {
+		return ~~(y / this.getItemHeight(0));
+	}
+
+	/* Internal Functions */
+	
+	function recycleOffscreenNodes() {
+	}
+	function recycleAllNodes() {
+		var inUse = table.inUse;
+		var recycle = table.recycle;
+		
+		for(var i = 0; i < inUse.length; i++) {
+			recycle.push(inUse[i]);
+		}
+		
+		table.inUse = [];
+	}
+
+	function grabNode() {
+		if(table.recycle.length > 0) {
+			return table.recycle.pop();
+		}
+		return false;
+	}
+	
+	// util, limit an event handler to only run every 20ms
+	function burnout(func) {
+		var burnedOut = false;
+		return function(evt) {
+			if(burnedOut) {
+				return;
+			}
+			burnedOut = true;
+			window.setTimeout(function() {burnedOut = false;}, 20);
+			return func.apply(this, evt);
+		}
+	}
+	
+	function renderSlice(startY, endY) {
+		var t = table.container;
+		t.empty();
+		
+		table.recalculate();
+		recycleAllNodes();
+		
+		// pad region
+		var height = table.getTotalHeight();
+		startY = Math.max(0, startY - SCROLL_BUFFER);
+		endY = Math.min(endY + SCROLL_BUFFER, height)
+		
+		var index = table.pixelToIndex(startY);
+		
+		for(var y = table.getItemTop(index); y < endY;) {
+			var h = table.getItemHeight(index);
+			var domNode = table.getComponent(index, grabNode());
+			domNode.css({
+				position: "absolute",
+				left: "0",
+				right: "0",
+				top: y
+			});
+			t.append(domNode);
+			index++;
+			y += h;
+		}
+		
+		table.container.css({
+			position: "relative",
+			height: height
+		});
+		//console.log(height + "pixels high")
+	}
+	
+	/* Public Methods */
+	
+	table.render = function() {
+		var screenY = $(window).scrollTop();
+		var tableY = this.container.offset().top;
+		var delta = screenY - tableY;
+		renderSlice(delta, delta + $(window).height());
+	}
+	
+	// utility for displaying fixed datasets of uniform row height
+	table.simpleArray = function(data, templateSelector, renderFunc) {
+		var tmpl;
+		var rowHeight;
+		
+		this.recalculate = function() {
+			tmpl = $(templateSelector);
+			rowHeight =tmpl.height();
+		}
+		
+		this.getComponent = function(index, node) {
+			if(!node) {
+				node = tmpl.clone();
+			}
+		
+			var dataItem = data[index];
+			renderFunc(dataItem, node, index);
+			
+			return node;
+		}
+		
+		this.getItemTop = function(index) {
+			return rowHeight*index;
+		}
+		
+		this.getItemHeight = function(index) {
+			return rowHeight;
+		}
+
+		this.getTotalHeight = function() {
+			return rowHeight * data.length;
+		}
+	}
+	
+	/* End */
+	
+	return table;
+}
