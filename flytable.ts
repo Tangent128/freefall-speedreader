aedd618c4ac42156791148386e67e1929703975b
Tangent 128 <Tangent128@gmail.com>
2019-09-25 00:35:29 -0400

    use class syntax & typings in `flytable.ts`

--- flytable.ts
+++ flytable.ts
@@ -1,10 +1,10 @@
 /* ISC Licensed:
- * 
- * Copyright (c) 2014-2018, Tangent128 
+ *
+ * Copyright (c) 2014-2018, Tangent128
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
  * above copyright notice and this permission notice appear in all copies.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
@@ -14,174 +14,181 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-function setupFlyTable(element) {
-	
-	var table = {};
-	table.container = element;
-	table.inUse = [];
-	table.sliceStart = 1/0;
-	table.sliceEnd = -1;
+class Flytable {
+  inUse = [] as JQuery[];
+  sliceStart = 1 / 0;
+  sliceEnd = -1;
 
-	// extra space on either side to render
-	table.scrollPadding = 10;
-	
-	/* Public Overrides */
-	
-	table.recalculate = function() {};
-	
-	table.getComponent = function(index) {
-		return $("<s>no renderer</s>");
-	}
-	
-	table.getItemTop = function(index) {
-		return this.getItemHeight(0)*index; // arbitrary value
-	}
-	
-	table.getItemHeight = function(index) {
-		return 16; // arbitrary value
-	}
-	
-	table.getTotalHeight = function() {
-		return 160; // arbitrary value
-	}
+  /** extra space on either side to render */
+  scrollPadding = 10;
 
-	table.pixelToIndex = function(y) {
-		return ~~(y / this.getItemHeight(0));
-	}
+  /* Public Overrides */
+  public recalculate = function(this: Flytable) {};
 
-	/* Internal Functions */
-	
-	function destroyOffscreenNodes(table, startY, endY) {
-		var inUse = table.inUse;
-		var stillInUse = [];
-		
-		var firstVisible = table.pixelToIndex(startY);
-		var lastVisible = table.pixelToIndex(endY);
-		
-		for(var i = 0; i < inUse.length; i++) {
-			var element = inUse[i];
-			var index = element.data("flytable-index");
-			
-			if(index < firstVisible || index > lastVisible) {
-				element.remove();
-			} else {
-				stillInUse.push(element);
-			}
-		}
-		
-		table.inUse = stillInUse;
-		table.sliceStart = Math.max(firstVisible, table.sliceStart);
-		table.sliceEnd = Math.min(table.sliceEnd, lastVisible);
-	}
-	
-	function includeInSlice(table, index) {
-		table.sliceStart = Math.min(index, table.sliceStart);
-		table.sliceEnd = Math.max(table.sliceEnd, index);
-	};
-	
-	function renderSlice(table, startY, endY) {
-		// prepare table element
-		var container = table.container;
-		table.recalculate();
-		var height = table.getTotalHeight();
+  public getComponent = function(this: Flytable, _index: number): JQuery {
+    return $("<s>no renderer</s>");
+  };
 
-		container.css({
-			position: "relative",
-			height: height
-		});
-		
-		// pad region
-		var startY = Math.max(0, startY - table.scrollPadding);
-		var endY = Math.min(endY + table.scrollPadding, height)
-		
-		// clear offscreen nodes
-		destroyOffscreenNodes(table, startY, endY);
+  public getItemTop = function(this: Flytable, index: number): number {
+    return this.getItemHeight(0) * index; // arbitrary value
+  };
 
-		// loop through visible blocks
-		var index = table.pixelToIndex(startY);
-		var existingSliceStart = table.sliceStart;
-		var existingSliceEnd = table.sliceEnd;
+  public getItemHeight = function(this: Flytable, _index: number): number {
+    return 16; // arbitrary value
+  };
 
-		for(var y = table.getItemTop(index); y < endY;) {
-			var h = table.getItemHeight(index);
+  public getTotalHeight = function(this: Flytable): number {
+    return 160; // arbitrary value
+  };
 
-			if(index < existingSliceStart || index > existingSliceEnd) {
-				var element = table.getComponent(index);
-				element.css({
-					position: "absolute",
-					left: "0",
-					right: "0",
-					top: y,
-					height: h
-				});
-				element.data("flytable-index", index);
-				includeInSlice(table, index);
-				container.append(element);
-				table.inUse.push(element);
-			}
-			
-			index++;
-			y += h;
-		}
-		
-	}
-	
-	/* Public Methods */
-	
-	table.render = function() {
-		var screenY = $(window).scrollTop();
-		var tableY = this.container.offset().top;
-		var delta = screenY - tableY;
-		renderSlice(table, delta, delta + $(window).height());
-	};
-	
-	// utility for displaying datasets of uniform row height
-	// renderFunc(index of item, jQuery-wrapped node to render to)
-	table.simpleDataset = function(getDataCount, templateSelector, renderFunc) {
-		var tmpl;
-		var rowHeight;
-		
-		this.recalculate = function() {
-			tmpl = $(templateSelector);
-			rowHeight = tmpl.height();
-		}
-		
-		this.getComponent = function(index) {
-			var node = tmpl.clone();
-		
-			renderFunc(index, node);
-			
-			return node;
-		}
-		
-		this.getItemTop = function(index) {
-			return rowHeight*index;
-		}
-		
-		this.getItemHeight = function(index) {
-			return rowHeight;
-		}
+  public pixelToIndex = function(this: Flytable, y: number): number {
+    return ~~(y / this.getItemHeight(0));
+  };
 
-		this.getTotalHeight = function() {
-			return rowHeight * getDataCount();
-		}
-	}
-	
-	// utility for displaying array-based datasets
-	// renderFunc(item value, jQuery-wrapped node to render to, index of item)
-	table.simpleArray = function(array, templateSelector, renderFunc) {
-		this.simpleDataset(
-			function() {return array.length;},
-			templateSelector,
-			function(index, node) {
-				renderFunc(array[index], node, index)
-			}
-		)
-	}
-	
-	/* End */
-	
-	return table;
+  constructor(public container: JQuery) {}
+
+  private destroyOffscreenNodes(startY: number, endY: number) {
+    const inUse = this.inUse;
+    const stillInUse = [];
+
+    const firstVisible = this.pixelToIndex(startY);
+    const lastVisible = this.pixelToIndex(endY);
+
+    for (let i = 0; i < inUse.length; i++) {
+      const element = inUse[i];
+      const index = element.data("flytable-index");
+
+      if (index < firstVisible || index > lastVisible) {
+        element.remove();
+      } else {
+        stillInUse.push(element);
+      }
+    }
+
+    this.inUse = stillInUse;
+    this.sliceStart = Math.max(firstVisible, this.sliceStart);
+    this.sliceEnd = Math.min(this.sliceEnd, lastVisible);
+  }
+
+  private includeInSlice(index: number) {
+    this.sliceStart = Math.min(index, this.sliceStart);
+    this.sliceEnd = Math.max(this.sliceEnd, index);
+  }
+
+  private renderSlice(startY: number, endY: number) {
+    // prepare table element
+    const container = this.container;
+    this.recalculate();
+    const height = this.getTotalHeight();
+
+    container.css({
+      position: "relative",
+      height: height
+    });
+
+    // pad region
+    startY = Math.max(0, startY - this.scrollPadding);
+    endY = Math.min(endY + this.scrollPadding, height);
+
+    // clear offscreen nodes
+    this.destroyOffscreenNodes(startY, endY);
+
+    // loop through visible blocks
+    let index = this.pixelToIndex(startY);
+    const existingSliceStart = this.sliceStart;
+    const existingSliceEnd = this.sliceEnd;
+
+    for (let y = this.getItemTop(index); y < endY; ) {
+      const h = this.getItemHeight(index);
+
+      if (index < existingSliceStart || index > existingSliceEnd) {
+        const element = this.getComponent(index);
+        element.css({
+          position: "absolute",
+          left: "0",
+          right: "0",
+          top: y,
+          height: h
+        });
+        element.data("flytable-index", index);
+        this.includeInSlice(index);
+        container.append(element);
+        this.inUse.push(element);
+      }
+
+      index++;
+      y += h;
+    }
+  }
+
+  public render() {
+    const screenY = $(window).scrollTop();
+    const tableY = (this.container.offset() as JQueryCoordinates).top;
+    const delta = screenY - tableY;
+    this.renderSlice(delta, delta + $(window).height());
+  }
+
+  /**
+   * utility for displaying datasets of uniform row height
+   * @param renderFunc index of item, jQuery-wrapped node to render to
+   */
+  public simpleDataset(
+    getDataCount: () => number,
+    templateSelector: string | JQuery | HTMLElement,
+    renderFunc: (index: number, node: JQuery) => void
+  ) {
+    let tmpl: JQuery;
+    let rowHeight: number;
+
+    this.recalculate = function() {
+      tmpl = $(templateSelector);
+      rowHeight = tmpl.height();
+    };
+
+    this.getComponent = function(index) {
+      const node = tmpl.clone();
+
+      renderFunc(index, node);
+
+      return node;
+    };
+
+    this.getItemTop = function(index) {
+      return rowHeight * index;
+    };
+
+    this.getItemHeight = function(index) {
+      return rowHeight;
+    };
+
+    this.getTotalHeight = function() {
+      return rowHeight * getDataCount();
+    };
+  }
+
+  /**
+   * utility for displaying array-based datasets
+   * @param renderFunc (item value, jQuery-wrapped node to render to, index of item)
+   */
+  public simpleArray<T>(
+    array: T[],
+    templateSelector: string | JQuery | HTMLElement,
+    renderFunc: (value: T, node: JQuery, index: number) => void
+  ) {
+    this.simpleDataset(
+      function() {
+        return array.length;
+      },
+      templateSelector,
+      function(index, node) {
+        renderFunc(array[index], node, index);
+      }
+    );
+  }
+}
+
+function setupFlyTable(element: JQuery): Flytable {
+  return new Flytable(element);
 }
 
 /*
